7c6c73857c48f6db246e03fa12dec63ac179c66b
Simone Moraes <simone.moraes@gmail.com>
2017-12-19 08:25:46 -0200

    testeando repositorio 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <div class="alert alert-success text-center">
+        <h1>Consultorio Médico Drº José Expedito Rodrigues</h1>
+    </div>
+    <div class="text-center">
         <h1>Bem vindo!!</h1>
     </div>
 @endsection
